0500ac3e9fb44ff2c6ffd4e6d7302dd942968f66
dmpierre <pdaixmoreux@gmail.com>
2025-11-13 12:35:31 +0100

    chore: comment out deposits and withdrawals, sorted out later

--- core/src/datastructures/deposits/mod.rs
+++ core/src/datastructures/deposits/mod.rs
@@ -1,16 +1,7 @@
 use std::marker::PhantomData;
 
-use ark_crypto_primitives::{
-    crh::poseidon::TwoToOneCRH,
-    merkle_tree::{Config, IdentityDigestConverter, MerkleTree},
-    sponge::Absorb,
-};
+use ark_crypto_primitives::merkle_tree::MerkleTree;
 use ark_ec::CurveGroup;
-use ark_ff::PrimeField;
-
-use crate::primitives::crh::TransactionCRH;
-
-use super::transaction::Transaction;
 
 pub type DepositTree<P> = MerkleTree<P>;
 
@@ -18,11 +9,12 @@ pub struct DepositTreeConfig<C: CurveGroup> {
     _c: PhantomData<C>,
 }
 
-impl<C: CurveGroup<BaseField: PrimeField + Absorb>> Config for DepositTreeConfig<C> {
-    type Leaf = Transaction<C>;
-    type LeafDigest = C::BaseField;
-    type LeafInnerDigestConverter = IdentityDigestConverter<C::BaseField>;
-    type InnerDigest = C::BaseField;
-    type LeafHash = TransactionCRH<C>;
-    type TwoToOneHash = TwoToOneCRH<C::BaseField>;
-}
+// TODO
+//impl<C: CurveGroup<BaseField: PrimeField + Absorb>> Config for DepositTreeConfig<C> {
+//    type Leaf = Transaction<C>;
+//    type LeafDigest = C::BaseField;
+//    type LeafInnerDigestConverter = IdentityDigestConverter<C::BaseField>;
+//    type InnerDigest = C::BaseField;
+//    type LeafHash = TransactionCRH<C>;
+//    type TwoToOneHash = TwoToOneCRH<C::BaseField>;
+//}
--- core/src/datastructures/withdrawals/mod.rs
+++ core/src/datastructures/withdrawals/mod.rs
@@ -1,16 +1,12 @@
 use std::marker::PhantomData;
 
-use ark_crypto_primitives::{
-    crh::poseidon::TwoToOneCRH,
-    merkle_tree::{Config, IdentityDigestConverter, MerkleTree},
-    sponge::Absorb,
-};
+use ark_crypto_primitives::merkle_tree::MerkleTree;
 use ark_ec::CurveGroup;
-use ark_ff::PrimeField;
 
-use crate::primitives::crh::TransactionCRH;
+// TODO
+// use crate::primitives::crh::TransactionCRH;
 
-use super::transaction::Transaction;
+// use super::transaction::Transaction;
 
 pub type WithdrawTree<P> = MerkleTree<P>;
 
@@ -18,11 +14,11 @@ pub struct WithdrawTreeConfig<C: CurveGroup> {
     _c: PhantomData<C>,
 }
 
-impl<C: CurveGroup<BaseField: PrimeField + Absorb>> Config for WithdrawTreeConfig<C> {
-    type Leaf = Transaction<C>;
-    type LeafDigest = C::BaseField;
-    type LeafInnerDigestConverter = IdentityDigestConverter<C::BaseField>;
-    type InnerDigest = C::BaseField;
-    type LeafHash = TransactionCRH<C>;
-    type TwoToOneHash = TwoToOneCRH<C::BaseField>;
-}
+//impl<C: CurveGroup<BaseField: PrimeField + Absorb>> Config for WithdrawTreeConfig<C> {
+//    type Leaf = Transaction<C>;
+//    type LeafDigest = C::BaseField;
+//    type LeafInnerDigestConverter = IdentityDigestConverter<C::BaseField>;
+//    type InnerDigest = C::BaseField;
+//    type LeafHash = TransactionCRH<C>;
+//    type TwoToOneHash = TwoToOneCRH<C::BaseField>;
+//}
