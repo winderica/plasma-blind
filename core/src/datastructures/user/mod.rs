1012af003f203e13cdfb54d39ecc483b9c729a45
dmpierre <pdaixmoreux@gmail.com>
2025-11-13 12:39:12 +0100

    chore: commenting out user logic for now

--- core/src/datastructures/user/mod.rs
+++ core/src/datastructures/user/mod.rs
@@ -1,16 +1,15 @@
 use ark_crypto_primitives::{
-    sponge::{poseidon::PoseidonConfig, Absorb},
     Error,
+    sponge::{Absorb, poseidon::PoseidonConfig},
 };
 use ark_ec::CurveGroup;
 use ark_ff::PrimeField;
-use ark_std::rand::Rng;
 use ark_std::UniformRand;
+use ark_std::rand::Rng;
 
 use super::{
     keypair::{KeyPair, Signature},
     noncemap::Nonce,
-    transaction::Transaction,
 };
 
 pub mod constraints;
@@ -29,10 +28,10 @@ pub struct User<C: CurveGroup> {
 }
 
 impl<
-        F: PrimeField + Absorb,
-        F2: PrimeField + Absorb,
-        C: CurveGroup<ScalarField = F, BaseField = F2>,
-    > User<C>
+    F: PrimeField + Absorb,
+    F2: PrimeField + Absorb,
+    C: CurveGroup<ScalarField = F, BaseField = F2>,
+> User<C>
 {
     pub fn new(rng: &mut impl Rng, id: UserId) -> Self {
         Self {
@@ -52,18 +51,18 @@ impl<
         self.keypair.sk.sign::<C>(pp, m, rng)
     }
 
-    pub fn spend_transaction(&mut self, tx: Transaction<C>) {
-        for utxo in tx.inputs.iter().filter(|utxo| !utxo.is_dummy) {
-            self.balance -= utxo.amount;
-        }
-        self.nonce.0 += 1;
-    }
-
-    pub fn receive_transaction(&mut self, tx: Transaction<C>) {
-        for utxo in tx.outputs.iter().filter(|utxo| !utxo.is_dummy) {
-            self.balance += utxo.amount;
-        }
-    }
+    //    pub fn spend_transaction(&mut self, tx: Transaction<C>) {
+    //        for utxo in tx.inputs.iter().filter(|utxo| !utxo.is_dummy) {
+    //            self.balance -= utxo.amount;
+    //        }
+    //        self.nonce.0 += 1;
+    //    }
+    //
+    //    pub fn receive_transaction(&mut self, tx: Transaction<C>) {
+    //        for utxo in tx.outputs.iter().filter(|utxo| !utxo.is_dummy) {
+    //            self.balance += utxo.amount;
+    //        }
+    //    }
 }
 
 pub fn sample_user<C: CurveGroup<BaseField: PrimeField + Absorb>>(rng: &mut impl Rng) -> User<C> {
