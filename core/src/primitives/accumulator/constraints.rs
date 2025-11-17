d4aaff32949c0fd31c0708e7b4c30aaa60722c42
dmpierre <pdaixmoreux@gmail.com>
2025-11-17 16:28:25 +0100

    feat: add poseidon accumulator

--- core/src/primitives/accumulator/constraints.rs
+++ core/src/primitives/accumulator/constraints.rs
@@ -1,4 +1,15 @@
-use ark_crypto_primitives::crh::{TwoToOneCRHScheme, TwoToOneCRHSchemeGadget};
+use std::marker::PhantomData;
+
+use ark_crypto_primitives::{
+    crh::{
+        TwoToOneCRHScheme, TwoToOneCRHSchemeGadget,
+        poseidon::{
+            TwoToOneCRH,
+            constraints::{CRHParametersVar, TwoToOneCRHGadget},
+        },
+    },
+    sponge::Absorb,
+};
 use ark_ff::PrimeField;
 use ark_r1cs_std::fields::fp::FpVar;
 use ark_relations::gr1cs::SynthesisError;
@@ -9,4 +20,20 @@ pub trait Accumulator<F: PrimeField, H: TwoToOneCRHScheme, T: TwoToOneCRHSchemeG
         prev: &FpVar<F>,
         value: &FpVar<F>,
     ) -> Result<FpVar<F>, SynthesisError>;
+}
+
+pub struct PoseidonAccumulatorVar<F: PrimeField> {
+    _f: PhantomData<F>,
+}
+
+impl<F: PrimeField + Absorb> Accumulator<F, TwoToOneCRH<F>, TwoToOneCRHGadget<F>>
+    for PoseidonAccumulatorVar<F>
+{
+    fn update(
+        pp: &CRHParametersVar<F>,
+        prev: &FpVar<F>,
+        value: &FpVar<F>,
+    ) -> Result<FpVar<F>, SynthesisError> {
+        TwoToOneCRHGadget::evaluate(pp, prev, value)
+    }
 }
